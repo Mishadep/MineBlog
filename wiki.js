74bebeb0bf48ea70ca2de48b4bd0c7ca70e363cd
Shokha Denis <dejavu1231765@gmail.com>
2026-04-05 14:43:30 +0200

    update

--- wiki.js
+++ wiki.js
@@ -37,3 +37,34 @@ startButton.addEventListener('click', function() {
    
     window.location.href = 'reading.html'; 
 });
+
+document.querySelector('.burger-menu').addEventListener('click', function() {
+    // Переключаем класс 'active' у выпадающего списка
+    const dropdown = document.querySelector('.dropdown-content');
+    
+    if (dropdown.style.display === 'block') {
+        dropdown.style.display = 'none';
+    } else {
+        dropdown.style.display = 'block';
+    }
+});
+
+// Закрыть меню, если кликнуть в любом другом месте экрана
+window.onclick = function(event) {
+    if (!event.target.matches('.burger-menu') && !event.target.matches('.burger-menu span')) {
+        const dropdown = document.querySelector('.dropdown-content');
+        if (dropdown.style.display === 'block') {
+            dropdown.style.display = 'none';
+        }
+    }
+};
+const clickSound = new Audio('templates/click.mp3');
+
+// Находим все большие кнопки
+const buttons = document.querySelectorAll('.big-clickable-button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        clickSound.play();
+    });
+});
